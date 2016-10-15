e5e3f1b3fa9079d000b07c35dc95e67d37e3f375
chensel1 <kyameron343@gmail.com>
2016-10-15 13:09:09 -0400

    New Student html and php

---
+++ newstudent.php
@@ -0,0 +1,8 @@
+<?php
+
+$newStudentName = string($_POST['newStudentName']);
+$newStudentEmail = string($_POST['newStudentEmail']);
+$newStudentPassword = string($_POST['newStudentPassword']);
+$newStudentMajor = string($_POST['newStudentMajor']);
+
+?>
