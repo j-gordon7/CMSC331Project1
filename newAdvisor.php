bb759e104d932e6a4d87d99d12bb437504fedc60
alewis2 <lightbringer96@gmail.com>
2016-10-15 13:20:07 -0400

    newAdvisor.php basic read in done

--- newAdvisor.php
+++ newAdvisor.php
@@ -1,1 +1,17 @@
-asd
+<?php
+
+$newAdvisorName = string($_POST['Name']);
+
+$newAdvisorCollege = string($_POST['college']);
+
+$newAdvisorOffice = string($_POST['office']);
+
+$newAdvisorEmail = string($_POST['email']);
+
+$newAdvisorPhone = string($_POST['phone']);
+
+$newAdvisorPassword = string($_POST['password']);
+
+$newAdvisorPasswordConfirm = string($_POST['passwordConfirm']);
+
+?>
