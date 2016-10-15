84287728847942bb242b07f232a05ceac71e7a43
alewis2 <lightbringer96@gmail.com>
2016-10-15 14:50:40 -0400

    need to finish line 24

--- newAdvisor.php
+++ newAdvisor.php
@@ -1,5 +1,10 @@
 <?php
 
+include('CommonMethods.php');
+
+$debug = true;
+$COMMON = new Common($debug);
+
 $newAdvisorName = string($_POST['Name']);
 
 $newAdvisorCollege = string($_POST['college']);
@@ -13,5 +18,9 @@ $newAdvisorPhone = string($_POST['phone']);
 $newAdvisorPassword = string($_POST['password']);
 
 $newAdvisorPasswordConfirm = string($_POST['passwordConfirm']);
+
+$sql = "INSERT INTO `AdvisorDatabase`(`Name`, `College`, `Office`, `Email`, `Phone Number`, `Password`) VALUES ($newAdvisorName,$newAdvisorCollege,$newAdvisorOffice,$newAdvisorEmail,$newAdvisorPhone,$newAdvisorPassword)";
+
+$rs = $COMMON->executeQuery($sql, $_SERVER[
 
 ?>
