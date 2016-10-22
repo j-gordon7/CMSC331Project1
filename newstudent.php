7a801acc20c43711567d018f2482fff93f2f8253
jpeg210 <gordon7@umbc.edu>
2016-10-22 16:39:40 -0400

    this works

--- newstudent.php
+++ newstudent.php
@@ -1,26 +1,22 @@
 <?php
-  //$newStudentID = string($_POST['newStudentID']);
-  //$newStudentFirstName = string($_POST['newStudentFirstName']);
-  //$newStudentMiddleInitial = string($_POST['newStudentMiddleInitial']);
-  //$newStudentLastName = string($_POST['newStudentLastName']);
-  //$newStudentEmail = string($_POST['newStudentEmail']);
-  //$newStudentPassword = string($_POST['newStudentPassword']);
-  //$newStudentMajor = string($_POST['newStudentMajor']);
+$newStudentID = (string)($_POST['newStudentID']);
+$newStudentFirstName = (string)($_POST['newStudentFirstName']);
+$newStudentMiddleInitial = (string)($_POST['newStudentMiddleInitial']);
+$newStudentLastName = (string)($_POST['newStudentLastName']);
+$newStudentEmail = (string)($_POST['newStudentEmail']);
+$newStudentPassword = (string)($_POST['newStudentPassword']);
+$newStudentMajor = (string)($_POST['newStudentMajor']);
 
 //var_dump($_POST); echo("<br>");
 
 include ('CommonMethods.php');
 
-$debug = true;
+$debug = false;
 
 $COMMON = new Common($debug);
 
-$sql = "insert into Student_Info (`StudentID`, `FirstName`, `MiddleInitial`,
- `LastName`, `Email`, `Password`, `Major`) values ('$_POST[newStudentID]',
-'$_POST[newStudentFirstName]','$_POST[newStudentMiddleInitial]',
-'$_POST[newStudentLastName]','$_POST[newStudentEmail]',
-'$_POST[newStudentPassword]','$_POST[newStudentMajor]')";
+$sql = "INSERT INTO `Student_Info`(`StudentID`, `FirstName`, `MiddleInitial`, `LastName`, `Email`, `Password`, `Major`) VALUES ('$newStudentID','$newStudentFirstName','$newStudentMiddleInitial','$newStudentLastName','$newStudentEmail','$newStudentPassword','$newStudentMajor')";
 
-$rs = $COMMON->executeQuery($sql, $_SEVER["SCRIPT_NAME"]);
+$rs = $COMMON->executeQuery($sql, $_SERVER['SCRIPT_NAME']);
 
 ?>
