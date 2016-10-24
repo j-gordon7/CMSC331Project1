2c63fe68b906c6087d27e48be34a55f6992f71d7
jpeg210 <gordon7@umbc.edu>
2016-10-23 23:28:55 -0400

    A

--- newstudent.php
+++ newstudent.php
@@ -7,8 +7,6 @@ $newStudentEmail = (string)($_POST['newStudentEmail']);
 $newStudentPassword = (string)($_POST['newStudentPassword']);
 $newStudentMajor = (string)($_POST['newStudentMajor']);
 
-//var_dump($_POST); echo("<br>");
-
 include ('CommonMethods.php');
 
 $debug = false;
@@ -18,5 +16,7 @@ $COMMON = new Common($debug);
 $sql = "INSERT INTO `Student_Info`(`StudentID`, `FirstName`, `MiddleInitial`, `LastName`, `Email`, `Password`, `Major`) VALUES ('$newStudentID','$newStudentFirstName','$newStudentMiddleInitial','$newStudentLastName','$newStudentEmail','$newStudentPassword','$newStudentMajor')";
 
 $rs = $COMMON->executeQuery($sql, $_SERVER['SCRIPT_NAME']);
+
+header('Location: newStudentAppointment.html');
 
 ?>
