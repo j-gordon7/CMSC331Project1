2d8f21d680c0af7bb485f7c47b9856abc8aa9285
chensel1 <kyameron343@gmail.com>
2016-10-24 21:08:01 -0400

    Commentation Pt1
    Added commentation for home and newstudent.html/.php

--- newstudent.php
+++ newstudent.php
@@ -1,4 +1,9 @@
 <?php
+
+include ('CommonMethods.php');
+$debug = false;
+
+/*Take in variables from newStudent.html*/
 $newStudentID = (string)($_POST['newStudentID']);
 $newStudentFirstName = (string)($_POST['newStudentFirstName']);
 $newStudentMiddleInitial = (string)($_POST['newStudentMiddleInitial']);
@@ -7,19 +12,18 @@ $newStudentEmail = (string)($_POST['newStudentEmail']);
 $newStudentPassword = (string)($_POST['newStudentPassword']);
 $newStudentMajor = (string)($_POST['newStudentMajor']);
 
+/*Start session with student email*/
 session_start();
 $_SESSION['Student_Email'] = $newStudentEmail;
 
-include ('CommonMethods.php');
-
-$debug = false;
-
+/*Connect to database*/
 $COMMON = new Common($debug);
 
+/*Insert new student object into student_info database*/
 $sql = "INSERT INTO `Student_Info`(`StudentID`, `FirstName`, `MiddleInitial`, `LastName`, `Email`, `Password`, `Major`) VALUES ('$newStudentID','$newStudentFirstName','$newStudentMiddleInitial','$newStudentLastName','$newStudentEmail','$newStudentPassword','$newStudentMajor')";
-
 $rs = $COMMON->executeQuery($sql, $_SERVER['SCRIPT_NAME']);
 
+/*Send user to new page*/
 header('Location: newStudentAppointment.html');
 
 ?>
